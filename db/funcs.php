7949b9f12901dbdad3b5d591aa05a19e7fe2dbb0
TheWriter <netto9903@gmail.com>
2023-09-17 10:46:44 -0300

    Add current progress properly

---
+++ db/funcs.php
@@ -0,0 +1,30 @@
+<?php
+
+class centralDatabase {
+    private dbData = (
+        "root" => "localhost",
+        "username" => "root",
+        "password" => "",
+        "database" => "TBA"
+    );
+    
+    static private conn;
+
+    public connect() {
+
+    }
+
+    public addMovie() {
+
+    }
+
+    public remMovie() {
+
+    }
+
+    public altMovie() {
+
+    }
+}
+
+?>
